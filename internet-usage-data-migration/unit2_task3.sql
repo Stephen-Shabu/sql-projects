60b363f85074210524fc33ab4b5dcc5ab8683009
Stephen-Shabu <swindle@live.co.uk>
2024-11-30 21:11:23 +0000

    - Added additional queries

--- internet-usage-data-migration/unit2_task3.sql
+++ internet-usage-data-migration/unit2_task3.sql
@@ -3,3 +3,11 @@ CREATE DATABASE internet_usage;
 USE internet_usage;
 
 SELECT * FROM recent_internetusage;
+
+EXPLAIN recent_internetusage;
+
+SELECT Region, Year_2014 FROM recent_internetusage 
+WHERE Year_2014 = (SELECT MAX(Year_2014) FROM recent_internetusage);
+
+SELECT SUM(Year_2014), SUM(Year_2015), SUM(Year_2016), SUM(Year_2017), SUM(Year_2018), SUM(Year_2019), SUM(Year_2020)
+FROM recent_internetusage;
